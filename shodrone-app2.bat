59325622c9a041f414b3e4d6082d75228d198996
José Ribeiro <1230927@isep.ipp.pt>
2025-04-03 23:16:30 +0100

    fixed shodrone-app2.bat file

--- shodrone-app2.bat
+++ shodrone-app2.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 echo Running shodrone-app2...
-SET BASE_CP=shodrone.app2/target/shodrone.app2-0.0.1.jar;shodrone.app2/target/dependency/*;
+SET BASE_CP=shodrone.app2/target/shodrone.app2-0.1.0.jar;shodrone.app2/target/dependency/*;
 
-java -cp %BASE_CP% shodrone.app2.App2
+java -cp %BASE_CP% hello2.HelloWorld
 
