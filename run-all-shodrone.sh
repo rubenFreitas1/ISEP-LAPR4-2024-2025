ac65b3627332e5e121954d0970da29c6c7e83d6c
José Ribeiro <1230927@isep.ipp.pt>
2025-04-04 11:46:18 +0100

    created scripts .sh to run individually all aplications, and to run all of them at the same time #9

---
+++ run-all-shodrone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting all Shodrone applications..."
+
+./shodrone-app1.sh
+./shodrone-app2.sh
+
+echo "All Shodrone applications have started."
+read -p "Press Enter to continue..."
---
+++ shodrone.app1/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"hello","c":"Greeter","l":"Greeter()","u":"%3Cinit%3E()"},{"p":"hello","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"hello","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"hello","c":"Greeter","l":"sayHello()"}];updateSearchResults();
