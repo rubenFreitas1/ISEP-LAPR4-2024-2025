1cd7fabd21480bd87131bbb95490360008877ae7
rafa <1231031@isep.ipp.pt>
2025-04-04 11:34:24 +0100

    #105 plantuml diagram script updated and functional

--- generate-plantuml-diagrams.bat
+++ generate-plantuml-diagrams.bat
@@ -1,14 +1,13 @@
-echo "LOG: Generate Plantuml Diagrams"
-exportFormat="svg"
-#monochrome="true"
-extra="-SdefaultFontSize=20"
-#extra="-SdefaultFontName=Times New Roman -SdefaultFontSize=10"
+echo off
+echo LOG: Generate Plantuml Diagrams
+exportFormat=png
+:: set monochrome=true
+set extra=-SdefaultFontSize=20
+:: set extra=-SdefaultFontName="Times New Roman" -SdefaultFontSize=10
 
-for aFile in `find docs -name "*.puml" -type f`;
-do
-  #-Smonochrome=$monochrome
-  echo "Processing file: $aFile"
-	java -jar libs/plantuml-1.2023.1.jar $extra -t$exportFormat $aFile 
-done
+for /r docs %%f in (*.puml) do (
+  echo Processing file: %%f
+	java -jar libs/plantuml-1.2023.1.jar %extra% -t%exportFormat% "%%f"
+)
 
-echo "Finished"
+echo Finished
