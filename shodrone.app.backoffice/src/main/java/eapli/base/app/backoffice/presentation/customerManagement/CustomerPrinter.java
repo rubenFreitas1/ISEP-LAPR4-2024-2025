feeceaa0c3b33155b75ca76120a47bcd6607d7c3
José Ribeiro <1230927@isep.ipp.pt>
2025-05-02 16:57:21 +0100

    created class CustomerPrinter to continue implementing US230 #87

---
+++ shodrone.app.backoffice/src/main/java/eapli/base/app/backoffice/presentation/customerManagement/CustomerPrinter.java
@@ -0,0 +1,11 @@
+package eapli.base.app.backoffice.presentation.customerManagement;
+
+import eapli.base.customerManagement.domain.Customer;
+import eapli.framework.visitor.Visitor;
+
+public class CustomerPrinter implements Visitor<Customer> {
+    @Override
+    public void visit(Customer visitee) {
+
+    }
+}
