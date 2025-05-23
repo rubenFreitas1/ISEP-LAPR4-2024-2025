38394ad873ac5993b1c6221bf7eeeaa2da8e2bc4
rafa <1231031@isep.ipp.pt>
2025-05-23 12:46:24 +0100

    Slight changes to the show proposal controller

--- shodrone.core/src/main/java/eapli/base/showProposalManagement/application/RegisterShowProposalController.java
+++ shodrone.core/src/main/java/eapli/base/showProposalManagement/application/RegisterShowProposalController.java
@@ -10,17 +10,23 @@ import eapli.base.showRequestManagement.application.ShowRequestManagementService
 import eapli.base.showRequestManagement.domain.GeoLocation;
 import eapli.base.showRequestManagement.domain.ShowRequest;
 import eapli.base.showRequestManagement.repositories.ShowRequestRepository;
+import eapli.base.usermanagement.domain.ExemploPasswordPolicy;
 import eapli.base.usermanagement.domain.Roles;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.PasswordPolicy;
+import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalTime;
 import java.util.Calendar;
 
 public class RegisterShowProposalController {
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final PasswordEncoder passwordEncoder = new PlainTextEncoder();
+    private final PasswordPolicy passwordPolicy = new ExemploPasswordPolicy();
     private final CustomerRepository customerRepository = PersistenceContext.repositories().customers();
-    private final CustomerManagementService customerManagementService = new CustomerManagementService(customerRepository);
+    private final CustomerManagementService customerManagementService = new CustomerManagementService(customerRepository, passwordEncoder, passwordPolicy);
     private final ShowRequestRepository showRequestRepository = PersistenceContext.repositories().showRequests();
     private final ShowRequestManagementService showRequestManagementService = new ShowRequestManagementService(showRequestRepository);
     private final ShowProposalRepository showProposalRepository = PersistenceContext.repositories().showProposals();
