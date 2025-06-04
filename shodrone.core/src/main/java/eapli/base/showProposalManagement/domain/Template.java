137182a11a88b0b161af12d5b0b3b016f200a2af
Ruben Freitas <1231267@isep.ipp.pt>
2025-06-04 23:14:49 +0100

    Changes in the pom to fix error

--- shodrone.core/src/main/java/eapli/base/showProposalManagement/domain/Template.java
+++ shodrone.core/src/main/java/eapli/base/showProposalManagement/domain/Template.java
@@ -1,10 +1,9 @@
 package eapli.base.showProposalManagement.domain;
 
 import eapli.framework.domain.model.AggregateRoot;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
 public class Template implements AggregateRoot<Long> {
@@ -15,6 +14,7 @@ public class Template implements AggregateRoot<Long> {
 
     private String name;
 
+    @Column(name = "CONTENT", length = 5000)
     private String content;
 
     protected Template() {
@@ -29,6 +29,28 @@ public class Template implements AggregateRoot<Long> {
     public String name (){return this.name;}
 
     public String content (){return this.content;}
+
+    @Override
+    public String toString() {
+        return "Template{" +
+                "templateId=" + templateId +
+                ", name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Template template = (Template) o;
+        return Objects.equals(templateId, template.templateId) && Objects.equals(name, template.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(templateId, name);
+    }
 
     @Override
     public boolean sameAs(Object other) {
